b430bcb25e1e992fc09b252a9ff3dbc1f38a83a9
Wings' might <wingsmight@gmail.com>
2019-11-15 21:37:04 +0700

    Add bottom navigation bar

--- app/src/main/java/com/wingsmight/makeday/MainActivity.java
+++ app/src/main/java/com/wingsmight/makeday/MainActivity.java
@@ -1,7 +1,11 @@
 package com.wingsmight.makeday;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -11,5 +15,49 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navigationListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MyDaysFragment()).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navigationListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener()
+            {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem)
+                {
+                    Fragment selectedFragment = null;
+
+                    switch (menuItem.getItemId())
+                    {
+                        case R.id.bottom_navigation_my_days0:
+                        {
+                            selectedFragment = new MyDaysFragment();
+                            break;
+                        }
+                        case R.id.bottom_navigation_tracker1:
+                        {
+                            selectedFragment = new TrackerFragment();
+                            break;
+                        }
+                        case R.id.bottom_navigation_growth2:
+                        {
+                            selectedFragment = new GrowthFragment();
+                            break;
+                        }
+                        case R.id.bottom_navigation_menu3:
+                        {
+                            selectedFragment = new MenuFragment();
+                            break;
+                        }
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
 }
