e287f658519511d6882adc6c5fd4e3b1c6b10d22
Wings' might <wingsmight@gmail.com>
2019-11-16 21:03:06 +0700

    add tab layout for tracker fragment

--- app/src/main/java/com/wingsmight/makeday/MainActivity.java
+++ app/src/main/java/com/wingsmight/makeday/MainActivity.java
@@ -1,63 +1,80 @@
 package com.wingsmight.makeday;
 
+import android.content.Context;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity
 {
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        context = getApplicationContext();
         setContentView(R.layout.activity_main);
 
+        SetupBottomNavigationView();
+    }
+
+    private void SetupBottomNavigationView()
+    {
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        BottomNavigationView.OnNavigationItemSelectedListener navigationListener =
+                new BottomNavigationView.OnNavigationItemSelectedListener()
+                {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem)
+                    {
+                        Fragment selectedFragment = null;
+
+                        switch (menuItem.getItemId())
+                        {
+                            case R.id.bottom_navigation_my_days0:
+                            {
+                                selectedFragment = new MyDaysFragment();
+                                break;
+                            }
+                            case R.id.bottom_navigation_tracker1:
+                            {
+                                selectedFragment = new TrackerFragment();
+                                break;
+                            }
+                            case R.id.bottom_navigation_growth2:
+                            {
+                                selectedFragment = new GrowthFragment();
+                                break;
+                            }
+                            case R.id.bottom_navigation_menu3:
+                            {
+                                selectedFragment = new MenuFragment();
+                                break;
+                            }
+                        }
+
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                                selectedFragment).commit();
+
+                        return true;
+                    }
+                };
+
+
         bottomNavigationView.setOnNavigationItemSelectedListener(navigationListener);
 
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MyDaysFragment()).commit();
     }
 
-    private BottomNavigationView.OnNavigationItemSelectedListener navigationListener =
-            new BottomNavigationView.OnNavigationItemSelectedListener()
-            {
-                @Override
-                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem)
-                {
-                    Fragment selectedFragment = null;
 
-                    switch (menuItem.getItemId())
-                    {
-                        case R.id.bottom_navigation_my_days0:
-                        {
-                            selectedFragment = new MyDaysFragment();
-                            break;
-                        }
-                        case R.id.bottom_navigation_tracker1:
-                        {
-                            selectedFragment = new TrackerFragment();
-                            break;
-                        }
-                        case R.id.bottom_navigation_growth2:
-                        {
-                            selectedFragment = new GrowthFragment();
-                            break;
-                        }
-                        case R.id.bottom_navigation_menu3:
-                        {
-                            selectedFragment = new MenuFragment();
-                            break;
-                        }
-                    }
 
-                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                            selectedFragment).commit();
 
-                    return true;
-                }
-            };
+    private static Context context;
+    public static Context GetContext()
+    {
+        return context;
+    }
 }
--- app/src/main/java/com/wingsmight/makeday/TrackerFragment.java
+++ app/src/main/java/com/wingsmight/makeday/TrackerFragment.java
@@ -3,17 +3,41 @@ package com.wingsmight.makeday;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.widget.LinearLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class TrackerFragment extends Fragment
 {
+    private PagerAdapterInTracker pagerAdapterInTracker;
+    private ViewPager viewPager;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
     {
         return inflater.inflate(R.layout.fragment_tracker, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState)
+    {
+        SetupTabLayout();
+    }
+
+    private void SetupTabLayout()
+    {
+        pagerAdapterInTracker = new PagerAdapterInTracker(getChildFragmentManager());
+
+        viewPager = getView().findViewById(R.id.view_pager);
+        viewPager.setAdapter(pagerAdapterInTracker);
+
+        TabLayout tabLayout = getView().findViewById(R.id.tracker_tabs);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
