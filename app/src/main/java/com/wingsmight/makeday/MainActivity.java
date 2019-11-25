1627a016e3fd2684a0c83d106546ae0c9932ab78
Wings' might <wingsmight@gmail.com>
2019-11-25 21:27:51 +0700

    save nav tabs states

--- app/src/main/java/com/wingsmight/makeday/MainActivity.java
+++ app/src/main/java/com/wingsmight/makeday/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
@@ -16,12 +17,25 @@ import com.wingsmight.makeday.Tracker.TrackerFragment;
 
 public class MainActivity extends AppCompatActivity
 {
+    final Fragment fragment1 = new MyDaysFragment();
+    final Fragment fragment2 = new TrackerFragment();
+    final Fragment fragment3 = new GrowthFragment();
+    final Fragment fragment4 = new MenuFragment();
+    final FragmentManager fragmentManager = getSupportFragmentManager();
+    Fragment active = fragment1;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         context = getApplicationContext();
         setContentView(R.layout.activity_main);
+
+        fragmentManager.beginTransaction().add(R.id.main_container, fragment4, "4").hide(fragment4).commit();
+        fragmentManager.beginTransaction().add(R.id.main_container, fragment3, "3").hide(fragment3).commit();
+        fragmentManager.beginTransaction().add(R.id.main_container, fragment2, "2").hide(fragment2).commit();
+        fragmentManager.beginTransaction().add(R.id.main_container,fragment1, "1").commit();
 
         SetupBottomNavigationView();
     }
@@ -36,43 +50,33 @@ public class MainActivity extends AppCompatActivity
                     @Override
                     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem)
                     {
-                        Fragment selectedFragment = null;
-
-                        switch (menuItem.getItemId())
-                        {
+                        switch (menuItem.getItemId()) {
                             case R.id.bottom_navigation_my_days0:
-                            {
-                                selectedFragment = new MyDaysFragment();
-                                break;
-                            }
-                            case R.id.bottom_navigation_tracker1:
-                            {
-                                selectedFragment = new TrackerFragment();
-                                break;
-                            }
-                            case R.id.bottom_navigation_growth2:
-                            {
-                                selectedFragment = new GrowthFragment();
-                                break;
-                            }
-                            case R.id.bottom_navigation_menu3:
-                            {
-                                selectedFragment = new MenuFragment();
-                                break;
-                            }
-                        }
+                                fragmentManager.beginTransaction().hide(active).show(fragment1).commit();
+                                active = fragment1;
+                                return true;
 
-                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                                selectedFragment).commit();
+                            case R.id.bottom_navigation_tracker1:
+                                fragmentManager.beginTransaction().hide(active).show(fragment2).commit();
+                                active = fragment2;
+                                return true;
+
+                            case R.id.bottom_navigation_growth2:
+                                fragmentManager.beginTransaction().hide(active).show(fragment3).commit();
+                                active = fragment3;
+                                return true;
+
+                            case R.id.bottom_navigation_menu3:
+                                fragmentManager.beginTransaction().hide(active).show(fragment4).commit();
+                                active = fragment4;
+                                return true;
+                        }
 
                         return true;
                     }
                 };
 
-
         bottomNavigationView.setOnNavigationItemSelectedListener(navigationListener);
-
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MyDaysFragment()).commit();
     }
 
     private static Context context;
